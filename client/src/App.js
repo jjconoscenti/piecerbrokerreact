eefce7dcda5c30d63481508a82bf99acc2c97aef
mccarthinator <mccarthy.cath@gmail.com>
2017-10-17 14:34:16 -0500

    formatting some stuff in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -53,30 +53,30 @@ class App extends Component {
         </Row>
 <Row>
         <Col m={12} l={12}>
-          <Footer copyrights="2017 Copyright PieceBroker"
-	moreLinks={
-		<a className="grey-text text-lighten-4 right" href="#!"></a>
-	}
-	links={
-		<ul>
-			<li><a className="grey-text text-lighten-3" href="https://github.com/dbalkin77">Doug Balkin</a></li>
-			<li><a className="grey-text text-lighten-3" href="https://github.com/jjconoscenti">JJ Conoscenti</a></li>
-			<li><a className="grey-text text-lighten-3" href="https://github.com/colinmac17">Colin McAtee</a></li>
-			<li><a className="grey-text text-lighten-3" href="https://github.com/mccarthinator">Katie McCarthy</a></li>
-            <li><a className="grey-text text-lighten-3" href="https://github.com/lisamishra">Lisa Mishra</a></li>
-            <li><a className="grey-text text-lighten-3" href="https://github.com/wjthornberry">Jake Thornberry</a></li>
-		</ul>
-	}
-	className='example'
->
-		<h5 className="white-text">PieceBroker</h5>
-		<p className="grey-text text-lighten-4">{`What's your mouth up for?`}</p>
-</Footer>
-</Col>
-        </Row>
-        </div>
-      );
-    }
+        
+        <Footer copyrights="2017 Copyright PieceBroker"
+          moreLinks={
+            <a className="grey-text text-lighten-4 right" href="#!"></a>
+          }
+          links={
+            <ul>
+              <li><a className="grey-text text-lighten-3" href="https://github.com/dbalkin77">Doug Balkin</a></li>
+              <li><a className="grey-text text-lighten-3" href="https://github.com/jjconoscenti">JJ Conoscenti</a></li>
+              <li><a className="grey-text text-lighten-3" href="https://github.com/colinmac17">Colin McAtee</a></li>
+              <li><a className="grey-text text-lighten-3" href="https://github.com/mccarthinator">Katie McCarthy</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/lisamishra">Lisa Mishra</a></li>
+                    <li><a className="grey-text text-lighten-3" href="https://github.com/wjthornberry">Jake Thornberry</a></li>
+            </ul>
+          }
+          className='example'>
+            <h5 className="white-text">PieceBroker</h5>
+            <p className="grey-text text-lighten-4">{`What's your mouth up for?`}</p>
+        </Footer>
+        </Col>
+</Row>
+                </div>
+              );
+            }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
