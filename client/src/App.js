0d86800e13c0fd8ae146482142096e609fa9179f
Douglas Balkin <dbalkin77@gmail.com>
2017-10-16 20:42:40 -0500

    displaying to DOM

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 import './App.css';
+import {Row, Col, Card, CardTitle} from 'react-materialize';
+
 
 /*class App extends Component {
   render() {
@@ -19,10 +21,61 @@ import './App.css';
   }
 }*/
 
+const dougBalkin = {
+  src: './teamImages/doug-balkin.jpg',
+  width: '243.05px',
+  height: '243.05px' 
+};
+
 class App extends Component {
     render() {
       return (
-        <div className="container">
+        <Row>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+        </Row>
+    
+        /*<div className="container">
             <div className="section">
                 <div className="row">
                     <div className="col s12">
@@ -125,7 +178,7 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>*/
       );
     }
 }
