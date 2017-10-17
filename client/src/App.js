cfbbab722005fcce43b83344c511e9c2d5ce98a6
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-10-16 22:03:32 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -84,3 +84,86 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // export default Team;
 
 export default App;
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import {Row, Col, Card, CardTitle, Footer} from 'react-materialize';
+
+class App extends Component {
+    render() {
+      return (
+        <div>
+        <Row>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+          <Col m={4} s={4}>
+            <Card header={<CardTitle reveal image={"./teamImages/doug-balkin.jpg"} waves='light'/>}
+              title="Doug Balkin"
+              reveal={<p>Subtly charming internet enthusiast and opera singer. Unapologetic thinker. Infuriatingly humble web aficionado. Avid tv advocate.</p>}>
+              <p><a href="https://github.com/dbalkin77">More about Doug</a></p>
+            </Card>
+          </Col>
+        </Row>
+<Row>
+        <Col m={12} l={12}>
+          <Footer copyrights="2017 Copyright PieceBroker"
+	moreLinks={
+		<a className="grey-text text-lighten-4 right" href="#!"></a>
+	}
+	links={
+		<ul>
+			<li><a className="grey-text text-lighten-3" href="https://github.com/dbalkin77">Doug Balkin</a></li>
+			<li><a className="grey-text text-lighten-3" href="https://github.com/jjconoscenti">JJ Conoscenti</a></li>
+			<li><a className="grey-text text-lighten-3" href="https://github.com/colinmac17">Colin McAtee</a></li>
+			<li><a className="grey-text text-lighten-3" href="https://github.com/mccarthinator">Katie McCarthy</a></li>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/lisamishra">Lisa Mishra</a></li>
+            <li><a className="grey-text text-lighten-3" href="https://github.com/wjthornberry">Jake Thornberry</a></li>
+		</ul>
+	}
+	className='example'
+>
+		<h5 className="white-text">PieceBroker</h5>
+		<p className="grey-text text-lighten-4">What's your mouth up for?</p>
+</Footer>
+</Col>
+        </Row>
+        </div>
+      );
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+// export default Team;
+export default App;
